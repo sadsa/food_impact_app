37553b3cdea00a03755d732e6135fc78ea311dce
Josh Biddick <josh.biddick@gmail.com>
2019-05-11 15:06:36 +1200

    implement state management for FoodModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+void main() {
+  var model = FoodModel();
+
+  runApp(ScopedModel<FoodModel>(
+    model: model,
+    child: MyApp(),
+  ));
+}
+
+class FoodModel extends Model {
+  List<Food> foods = new List<Food>();
+
+  addFoods(Iterable<Food> newFoods) {
+    foods.addAll(newFoods);
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -26,32 +46,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    _setFoods();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[FoodSelectorForm()],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: _buildBody());
+  }
+
+  _buildBody() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[FoodSelectorForm()],
       ),
     );
+  }
+
+  _setFoods() {
+    FoodDataService.getFoods().then((value) {
+      ScopedModel.of<FoodModel>(context).addFoods(value);
+    });
   }
 }
 
@@ -64,6 +86,7 @@ class FoodSelectorForm extends StatefulWidget {
 
 class FoodSelectorFormState extends State<FoodSelectorForm> {
   final _formKey = GlobalKey<FormState>();
+
   List<String> _foods = [
     "Farmed fishy",
     "Farmed prawns",
@@ -99,6 +122,11 @@ class FoodSelectorFormState extends State<FoodSelectorForm> {
           ),
           Text(_selectedFood.toString()),
           Text(_selectedFrequency.toString()),
+          ScopedModelDescendant<FoodModel>(
+            builder: (context, child, model) => model.foods.length > 0
+                ? Text(model.foods.first.name)
+                : LinearProgressIndicator(),
+          )
         ],
       ),
     );
@@ -123,5 +151,44 @@ class FoodSelectorFormState extends State<FoodSelectorForm> {
     setState(() {
       _selectedFrequency = value;
     });
+  }
+}
+
+class Food {
+  final String key;
+  String name;
+  String servingSize; //": "140g",
+  double ghg; //": "1.876796141",
+  double landUse; //": "1.158023151",
+  double waterUse; //": "508.277153",
+  String foodGroup; //": "Protein-rich foods",
+  List<String> sentences; //": "GHG.sentence.1, GHG.sentence.2, Water.sentence",
+  double avgServingsGlobal; //": "0.369237436",
+  double avgServingsUk; //": "0.390283225"
+
+  Food.fromJson(this.key, Map data) {
+    name = data['food'] != null ? data['food'] : '';
+  }
+}
+
+class FoodDataService {
+  // FoodDataService.getFoods().then(_updateFoods);
+  static Future<List<Food>> getFoods() async {
+    Completer<List<Food>> completer = new Completer<List<Food>>();
+
+    FirebaseDatabase.instance
+        .reference()
+        .child("Foods")
+        .once()
+        .then((DataSnapshot snapshot) {
+      Map<dynamic, dynamic> values = snapshot.value;
+      var foods = new List<Food>();
+      values.forEach((key, value) {
+        foods.add(new Food.fromJson(key, value));
+      });
+      completer.complete(foods);
+    });
+
+    return completer.future;
   }
 }
